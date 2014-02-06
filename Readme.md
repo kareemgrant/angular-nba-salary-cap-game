b89383a2b93b92f49e08ca3e5f56200d88b163bd
Kareem Grant <kareem@getuserwise.com>
2014-02-06 13:40:58 -0500

    Initial commit.

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Angular NBA Salary Cap Game
+
+## Build a competitve roster of NBA players without going over the salary cap
+
+### This app uses AngularJs to make it all happen
---
+++ create_team.js
@@ -0,0 +1,102 @@
+var league = {};
+
+league.players = [
+  {
+    name: 'Kobe Bryant',
+    team: 'Lakers',
+    salary:  30453805
+  },
+  {
+    name: 'Dirk Nowitzki',
+    team: 'Mavericks',
+    salary:  22721381
+  },
+  {
+    name: 'Amare Stoudemire',
+    team: 'Knicks',
+    salary:  21679893
+  },
+  {
+    name: 'Joe Johnson',
+    team: 'Nets',
+    salary:  21466718
+  },
+  {
+    name: 'Carmelo Anthony',
+    team: 'Knicks',
+    salary:  21388953
+  },
+  {
+    name: 'Dwight Howard',
+    team: 'Rockets',
+    salary:  20513178
+  },
+  {
+    name: 'Pau Gasol',
+    team: 'Lakers',
+    salary:  19285850
+  },
+  {
+    name: 'Chris Bosh',
+    team: 'Heat',
+    salary:  19067500
+  },
+  {
+    name: 'Lebron James',
+    team: 'Heat',
+    salary:  19067500
+  },
+
+  {
+    name: 'Dwayne Wade',
+    team: 'Heat',
+    salary:  18673000
+  },
+  {
+    name: 'Chris Paul',
+    team: 'Clippers',
+    salary:  18668431
+  },
+  {
+    name: 'Deron Williams',
+    team: 'Nets',
+    salary:  18466130
+  },
+  {
+    name: 'Zach Randolph',
+    team: 'Grizzilies',
+    salary:  18238333
+  },
+  {
+    name: 'Rudy Gay',
+    team: 'Kings',
+    salary:  17888932
+  },
+  {
+    name: 'Kevin Durant',
+    team: 'Thunder',
+    salary:  17832627
+  },
+];
+
+angular.module('team', [])
+  .controller('CreateTeamCtrl', function($scope){
+    $scope.players = league.players;
+    $scope.members = [];
+    $scope.teamSalary = 0;
+    var maxSalary = 100000000;
+
+    $scope.addPlayer = function(player){
+      $scope.teamSalary = player.salary + $scope.teamSalary;
+      return $scope.members.push(player);
+    }
+
+    $scope.removePlayer = function(index){
+      $scope.teamSalary = $scope.teamSalary - $scope.members[index].salary;
+      return $scope.members.splice(index, 1);
+    }
+
+    $scope.exceedsCap = function(){
+      return $scope.teamSalary > maxSalary;
+    };
+  });
