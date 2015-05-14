156ada1d94ee558af0dec44eeab3f562f34fd535
Kareem Grant <kareem@getuserwise.com>
2015-05-14 12:31:49 -0400

    Prevents players from being selected more than once

--- js/create_team.js
+++ js/create_team.js
@@ -1,105 +1,57 @@
 'use strict';
 
-var league = {};
-
-league.players = [
-  {
-    name: 'Kobe Bryant',
-    team: 'Lakers',
-    salary:  30453805
-  },
-  {
-    name: 'Dirk Nowitzki',
-    team: 'Mavericks',
-    salary:  22721381
-  },
-  {
-    name: 'Amare Stoudemire',
-    team: 'Knicks',
-    salary:  21679893
-  },
-  {
-    name: 'Joe Johnson',
-    team: 'Nets',
-    salary:  21466718
-  },
-  {
-    name: 'Carmelo Anthony',
-    team: 'Knicks',
-    salary:  21388953
-  },
-  {
-    name: 'Dwight Howard',
-    team: 'Rockets',
-    salary:  20513178
-  },
-  {
-    name: 'Pau Gasol',
-    team: 'Lakers',
-    salary:  19285850
-  },
-  {
-    name: 'Chris Bosh',
-    team: 'Heat',
-    salary:  19067500
-  },
-  {
-    name: 'Lebron James',
-    team: 'Heat',
-    salary:  19067500
-  },
-
-  {
-    name: 'Dwayne Wade',
-    team: 'Heat',
-    salary:  18673000
-  },
-  {
-    name: 'Chris Paul',
-    team: 'Clippers',
-    salary:  18668431
-  },
-  {
-    name: 'Deron Williams',
-    team: 'Nets',
-    salary:  18466130
-  },
-  {
-    name: 'Zach Randolph',
-    team: 'Grizzilies',
-    salary:  18238333
-  },
-  {
-    name: 'Rudy Gay',
-    team: 'Kings',
-    salary:  17888932
-  },
-  {
-    name: 'Kevin Durant',
-    team: 'Thunder',
-    salary:  17832627
-  },
-];
-
 angular.module('app', ['players'])
   .controller('CreateTeamCtrl', function($scope, playerList){
+
     $scope.players = playerList.getAllPlayers();
-    console.log($scope.players);
     $scope.members = [];
     $scope.teamSalary = 0;
+    $scope.memberCount = 0;
+
     var maxSalary = 100000000;
 
     $scope.addPlayer = function(player){
-      $scope.teamSalary = player.salary + $scope.teamSalary;
-      return $scope.members.push(player);
+      if($scope.exceedsMaxPlayers()){
+        console.log('Too many players');
+      }else {
+        $scope.teamSalary = player.salary + $scope.teamSalary;
+        $scope.memberCount = $scope.memberCount + 1;
+        player.selected = true;
+        console.log($scope.memberCount);
+        return $scope.members.push(player);
+      }
     }
 
     $scope.removePlayer = function(index){
-      $scope.teamSalary = $scope.teamSalary - $scope.members[index].salary;
+      var player = $scope.members[index];
+      $scope.teamSalary = $scope.teamSalary - player.salary;
+      player.selected = false;
+      $scope.memberCount--
       return $scope.members.splice(index, 1);
     }
 
     $scope.exceedsCap = function(){
       return $scope.teamSalary > maxSalary;
+    };
+
+    $scope.clearRoster = function(){
+      resetSelections();
+      $scope.members = [];
+      $scope.memberCount = 0;
+      $scope.teamSalary = 0;
+    };
+
+    $scope.preventDuplicates = {
+
+    }
+
+    function resetSelections(){
+      $scope.members.forEach(function(element, index, ary){
+        ary[index].selected = false;
+      });
+    };
+
+    $scope.exceedsMaxPlayers = function(){
+      return $scope.memberCount >= 8;
     };
   });
